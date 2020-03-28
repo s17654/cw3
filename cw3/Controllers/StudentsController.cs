8c529735c6f0c259c18147b7df11615bede25768
Łukasz Markowski <s17654@pjwstk.edu.pl>
2020-03-28 21:56:45 +0100

    cw3 done

---
+++ cw3/Controllers/StudentsController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using cw3.DAL;
+using cw3.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+
+        //[HttpGet]
+        //public string GetStudent()
+        //{
+        //    return "Kowalski, Malewski, Andrzejewski";
+        //}
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudents(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }
+            else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+
+            return NotFound("Nie znaleziono studenta");
+        }
+
+        //[HttpGet]
+        //public string GetStudents(string orderBy)
+        //{
+        //    return $"Kowalski, Malewski, Andrzejewski sortowanie={orderBy}";
+        //}
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult updateStudent(int id)
+        {
+            return Ok("Aktualizacja dokończona");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult deleteStudent(int id)
+        {
+            return Ok("Usuwanie zakończone");
+        }
+    }
+}
