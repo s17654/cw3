7b8c996a58dba238da3fb1a03eb87c8eb8fa46c0
s17654 <s17654@pjwstk.edu.pl>
2020-04-19 17:57:58 +0200

    Cw4 final

---
+++ cw3/Services/IStudentsDal.cs
@@ -0,0 +1,13 @@
+﻿using cw3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw3.Services
+{
+    public interface IStudentsDal
+    {
+        public IEnumerable<Student> GetStudents();
+    }
+}
---
+++ cw3/Services/SqlServerDbDal.cs
@@ -0,0 +1,16 @@
+﻿using cw3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw3.Services
+{
+    public class SqlServerDbDal : IStudentsDal
+    {
+        public IEnumerable<Student> GetStudents()
+        {
+            return null;
+        }
+    }
+}
